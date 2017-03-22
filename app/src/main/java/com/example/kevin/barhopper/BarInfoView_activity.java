9a65a8ee9a2fcc8affc529fbec2f02eb5c46cb59
KevinKabaria <kevin.kabaria@gmail.com>
2017-03-22 15:31:13 -0400

    Filled in new view.
    Now when a bar is clicked in the listview, takes us to a new page with bare-bones information (right now).
    
    Displays rating stars, pricing in stars (if available), name and address.
    
    Having a lot of trouble with ratingbar, so might have to make a custom one down the road.

--- app/src/main/java/com/example/kevin/barhopper/BarInfoView_activity.java
+++ app/src/main/java/com/example/kevin/barhopper/BarInfoView_activity.java
@@ -1,7 +1,10 @@
 package com.example.kevin.barhopper;
 
+import android.media.Rating;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.gms.location.places.Place;
 
@@ -20,6 +23,30 @@ public class BarInfoView_activity extends AppCompatActivity {
     }
 
     public void createUI(Place p) {
+        CharSequence titleS = p.getName();
+        float ratingF = p.getRating();
+        int priceI = p.getPriceLevel();
+        CharSequence descriptionS = p.getAddress();
+
+        TextView title = (TextView) findViewById(R.id.titleText);
+        title.setText(p.getName());
+
+        RatingBar rateBar = (RatingBar) findViewById(R.id.ratingBar);
+        rateBar.setRating(ratingF);
+        System.out.println("RATING!: " + ratingF);
+
+        TextView price = (TextView) findViewById(R.id.priceText);
+        String priceBuilder = "";
+        System.out.println("Price!: " + priceI);
+
+        for (int i = 0; i < priceI; i++) {
+            priceBuilder += "$";
+        }
+        price.setText(priceBuilder);
+
+        TextView description = (TextView) findViewById(R.id.descriptionText);
+        description.setText(descriptionS);
+
 
     }
 }
